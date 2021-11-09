7420314a6026a2270da1c1f3ecb3b311b93a43ba
henriquemsouza <henrique.m_s@hotmail.com>
2021-11-09 20:45:35 -0300

    refactor: just a basic change

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, deprecated_member_use, prefer_final_fields
 
-import 'dart:math';
+import 'package:first_project/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,63 +10,3 @@ void main() {
   ));
 }
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  var _phrase = ["lorem 1", "lorem 2", "lorem 3", "lorem 4"];
-  var _phraseGenerated = "Clique abaixo para gerar uma frase";
-
-  void _generatePhrase() {
-    var sortedNumber = Random().nextInt(_phrase.length);
-
-    setState(() {
-      _phraseGenerated = _phrase[sortedNumber];
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Frases do dia"),
-          backgroundColor: Colors.black12,
-        ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(16),
-            width: double.infinity,
-            decoration: BoxDecoration(
-                border: Border.all(width: 3, color: Colors.amber)),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset("images/logo.png"),
-                  Text(
-                    _phraseGenerated,
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                        fontSize: 17,
-                        fontStyle: FontStyle.italic,
-                        color: Colors.black),
-                  ),
-                  RaisedButton(
-                      child: Text(
-                        "Nova Frase",
-                        style: TextStyle(
-                            fontSize: 17,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      color: Colors.green,
-                      onPressed: _generatePhrase)
-                ]),
-          ),
-        ));
-  }
-}
